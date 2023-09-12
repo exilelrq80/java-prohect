5c2dffc46c2cbcf42b31a19c86cbfbed02c9a56f
Maha Alzhrani <139656620+exilelrq80@users.noreply.github.com>
2023-09-12 08:09:15 +0300

    Update Arearectangal1.java

--- Arearectangal1.java
+++ Arearectangal1.java
@@ -1,9 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package arearectangal1;
-
 import java.util.Scanner;
 public class Arearectangal1 {
     public static void main(String[] args) {
