bdf04ada5d286618ec6e7b98ed176a37520b6a6b
Maha Alzhrani <139656620+exilelrq80@users.noreply.github.com>
2023-10-31 23:02:20 +0300

    Create Q1.java
    مسأله عن ال method

---
+++ Q1.java
@@ -0,0 +1,13 @@
+public class Ex1 {
+    public static void main(String[] args) {
+        String Y = "I LOVE JAVA";
+        PrintDetalis(Y);  
+    }
+    public static void PrintDetalis(String Y){
+        System.out.println(Y.length());
+        System.out.println(Y.indexOf("JA"));
+        System.out.println(Y.toLowerCase());
+        System.out.println(Y.charAt(4));
+    }
+    
+}
